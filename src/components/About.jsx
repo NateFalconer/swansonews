6f2fe02632af126557aaf09a64c3a735117b1b90
Nate Falconer <falconerresources@gmail.com>
2020-02-27 16:38:34 -0500

    changes to links

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,9 +10,9 @@ class About extends Component {
             <br />
             <p>It works by fetching a random quote from a Ron Swanson quote API, then filtering the quote into searchable terms and plugging those terms into a search in The Guardian's API. It then scrapes the news article data and formats the text so the user can read one of three articles without ever leaving Swansonews.</p>
             <p>This project was developed by Nate Falconer.</p>
-            <a href="https://github.com/jamesseanwright/ron-swanson-quotes">Ron Swanson Quotes API</a> <br />
-            <a href="https://open-platform.theguardian.com/">The Guardian Open Platform</a> <br />
-            <a href="https://github.com/NateFalconer/react-api-project">GitHub</a>
+            <a href="https://github.com/jamesseanwright/ron-swanson-quotes" target="_blank">Ron Swanson Quotes API</a> <br />
+            <a href="https://open-platform.theguardian.com/" target="_blank">The Guardian Open Platform</a> <br />
+            <a href="https://github.com/NateFalconer/react-api-project" target="_blank">GitHub</a>
             </div>
             <img className="pyr" alt="pyra" src="./assets/pyramid.png" /> <br />
             <img className="dance" alt="dancin" src="./assets/dancin.gif" />
