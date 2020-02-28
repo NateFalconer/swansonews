8ddc1e959847839981833437bccbbbdd5d703b8d
Nate Falconer <falconerresources@gmail.com>
2020-02-27 23:59:50 -0500

    article changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav } from 'react-bootstrap';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class NavBar extends Component {
   render() {
@@ -11,11 +11,12 @@ class NavBar extends Component {
           <Navbar.Brand href="/">Give me more Swansonews!</Navbar.Brand>
           <Nav className="navLinks">
             <Link className="navlink" to="/about">What the hell is this?</Link>
+            <a href="https://www.youtube.com/watch?v=Tch4v0L0GHA" target="_blank" className="ytlink">Best of Ron Swanson</a>
           </Nav>
         </Navbar>
       </div>
-    );
-  }
-}
-
+        );
+      }
+    }
+    
 export default NavBar;
